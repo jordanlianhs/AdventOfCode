f5ca03557747feb1ca4f9d5ba0cd788b99d30cc9
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2023-12-30 22:49:36 +0800

    Day 21 and 22

--- 2023/21/Part 1.py
+++ 2023/21/Part 1.py
@@ -1,12 +1,35 @@
 import os
+from collections import deque
 
 # Get the directory of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to the file
-file_path = os.path.join(script_dir, "test.txt")
+file_path = os.path.join(script_dir, "day21.txt")
 
 # Now open the file
 try:
-    file = open(file_path).read().strip().split("\n")
+    grid = open(file_path).read().splitlines()
 except FileNotFoundError:
     print(f"The file {file_path} does not exist.")
+
+sr, sc = next((r,c) for r , row in enumerate(grid) for c, ch in enumerate(row) if ch == "S")
+
+ans = set()
+seen = {(sr, sc)}
+q = deque([(sr, sc, 64)])
+
+while q:
+    r, c, s = q.popleft()
+
+    if s % 2 == 0:
+        ans.add((r, c))
+    if s == 0:
+        continue
+
+    for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+        if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid[0]) or grid[nr][nc] == "#" or (nr, nc) in seen:
+            continue
+        seen.add((nr, nc))
+        q.append((nr, nc, s - 1))
+
+print(len(ans))
--- 2023/22/Part 1.py
+++ 2023/22/Part 1.py
@@ -1,12 +1,49 @@
 import os
+from collections import deque
 
 # Get the directory of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to the file
-file_path = os.path.join(script_dir, "test.txt")
+file_path = os.path.join(script_dir, "day22.txt")
 
 # Now open the file
 try:
-    file = open(file_path).read().strip().split("\n")
+    file = open(file_path)
 except FileNotFoundError:
     print(f"The file {file_path} does not exist.")
+
+bricks = [list(map(int, line.replace("~", ",").split(","))) for line in file]
+print(bricks)
+# Sort by the z coordinate
+bricks.sort(key=lambda brick: brick[2])
+print(bricks)
+
+def overlaps(a, b):
+    return max(a[0], b[0]) <= min(a[3], b[3]) and max(a[1], b[1]) <= min(a[4], b[4])
+
+for index, brick in enumerate(bricks):
+    max_z = 1
+    for check in bricks[:index]:
+        if overlaps(brick, check):
+            max_z = max(max_z, check[5] + 1)
+    brick[5] -= brick[2] - max_z
+    brick[2] = max_z
+    
+bricks.sort(key=lambda brick: brick[2])
+
+k_supports_v = {i: set() for i in range(len(bricks))}
+v_supports_k = {i: set() for i in range(len(bricks))}
+
+for j, upper in enumerate(bricks):
+    for i, lower in enumerate(bricks[:j]):
+        if overlaps(lower, upper) and upper[2] == lower[5] + 1:
+            k_supports_v[i].add(j)
+            v_supports_k[j].add(i)
+
+total = 0
+
+for i in range(len(bricks)):
+    if all(len(v_supports_k[j]) >= 2 for j in k_supports_v[i]):
+        total += 1
+
+print(total)
