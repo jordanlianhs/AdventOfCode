37a3ef0f4463949638b3bf6a275e18148eab9a86
Lian Hong Shen Jordan <97859704+jordanlianhs@users.noreply.github.com>
2024-01-01 21:23:21 +0800

    Day 24 and 25

--- 2023/24/Part 2.py
+++ 2023/24/Part 2.py
@@ -1,12 +1,33 @@
 import os
+import sympy
 
 # Get the directory of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to the file
-file_path = os.path.join(script_dir, "test.txt")
+file_path = os.path.join(script_dir, "day24.txt")
 
 # Now open the file
 try:
-    file = open(file_path).read().strip().split("\n")
+    file = open(file_path)
 except FileNotFoundError:
     print(f"The file {file_path} does not exist.")
+
+hailstones = [tuple(map(int, line.replace("@", ",").split(","))) for line in file]
+
+xr, yr, zr, vxr, vyr, vzr = sympy.symbols("xr, yr, zr, vxr, vyr, vzr")
+
+equations = []
+
+for i, (sx, sy, sz, vx, vy, vz) in enumerate(hailstones):
+    equations.append((xr - sx) * (vy - vyr) - (yr - sy) * (vx - vxr))
+    equations.append((yr - sy) * (vz - vzr) - (zr - sz) * (vy - vyr))
+    if i < 2:
+        continue
+    answers = [soln for soln in sympy.solve(equations) if all(x % 1 == 0 for x in soln.values())]
+    if len(answers) == 1:
+        break
+    
+answer = answers[0]
+
+print(answer[xr] + answer[yr] + answer[zr])
+print(i)
--- 2023/25/Part 1.py
+++ 2023/25/Part 1.py
@@ -1,12 +1,26 @@
 import os
+import networkx as nx
 
 # Get the directory of the script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to the file
-file_path = os.path.join(script_dir, "test.txt")
+file_path = os.path.join(script_dir, "day25.txt")
 
 # Now open the file
 try:
-    file = open(file_path).read().strip().split("\n")
+    file = open(file_path)
 except FileNotFoundError:
     print(f"The file {file_path} does not exist.")
+
+g = nx.Graph()
+
+for line in file:
+    left, right = line.split(":")
+    for node in right.strip().split():
+        g.add_edge(left, node)
+        g.add_edge(node, left)
+
+g.remove_edges_from(nx.minimum_edge_cut(g))
+a, b = nx.connected_components(g)
+
+print(len(a) * len(b))
